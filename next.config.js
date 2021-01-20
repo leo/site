c610416f9dfc168736516ab500bc6d392f58cc95
Leo Lamprecht <leo@vercel.com>
2021-01-20 11:16:27 +0100

    Fix RES and add prettier

---
+++ next.config.js
@@ -0,0 +1,13 @@
+const { getRedirectStatus } = require("next/dist/lib/load-custom-routes");
+
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: "/2017/electron-next",
+        destination: "https://github.com/leo/electron-next",
+        permanent: true,
+      },
+    ];
+  },
+};
