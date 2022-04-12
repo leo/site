520cbb53601f30a74f842ab8e617db093d3cb791
Leo Lamprecht <leo@vercel.com>
2022-04-12 23:57:14 +0200

    Even more TailwindCSS

--- components/link.tsx
+++ components/link.tsx
@@ -6,7 +6,7 @@ const Link = ({ children, href, onMouseEnter, onMouseLeave }) => (
     target="_blank"
     onMouseEnter={onMouseEnter}
     onMouseLeave={onMouseLeave}
-    className="text-base no-underline text-black p-2.5 block md:inline-block md:rounded-md md:py-1 md:px-2.5 md:mx-1.5 md:transition md:duration-200 md:hover:bg-neutral-200/60"
+    className="text-base no-underline text-black p-2.5 block md:rounded-md md:py-1 md:px-2.5 md:mx-1.5 md:transition md:duration-200 md:hover:bg-neutral-200/60"
   >
     {children}
   </a>
--- pages/index.tsx
+++ pages/index.tsx
@@ -35,13 +35,15 @@ const Index = () => {
       </Head>
 
       <section className="md:absolute md:inset-0 md:text-center md:flex md:h-full md:items-center md:justify-center md:select-none md:cursor-default">
-        <h2 className="text-2xl hidden md:inline-block">Just call me</h2>
+        <h2 className="text-2xl hidden md:inline-block md:font-light md:mt-2 md:mr-10 md:ml-0 md:mb-0">
+          Just call me
+        </h2>
         <h1 className="text-6xl font-light m-0 absolute top-9 left-9 pr-9 md:relative md:inline-block md:left-auto md:top-auto md:pr-0 md:font-light md:text-7xl">
           Leo
         </h1>
       </section>
 
-      <nav className="absolute left-7 bottom-7">
+      <nav className="absolute left-7 bottom-7 flex flex-col md:left-8 md:right-8 md:bottom-8 md:flex-row md:justify-center">
         <Link href="https://vercel.com">Mission</Link>
         <Link href={twitterURL}>Thoughts</Link>
         <Link href="https://github.com/leo">Code</Link>
@@ -74,20 +76,6 @@ const Index = () => {
               content: '„';
               left: -15px;
               bottom: 0;
-            }
-
-            section h2 {
-              margin: 8px 40px 0 0;
-              font-weight: 300;
-            }
-
-            nav {
-              left: 30px;
-              right: 30px;
-              white-space: nowrap;
-              text-align: center;
-              bottom: 30px;
-              font-size: 0;
             }
           }
 
