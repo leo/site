20d6c6544da1cdcb6b1eaddf83cbc166d5ed3813
Leo Lamprecht <leo.lamprecht@icloud.com>
2023-02-09 16:14:49 +0100

    Use arrow function for root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,10 +16,12 @@ interface RootLayoutProps {
   children: ReactNode;
 }
 
-export default function RootLayout({ children }: RootLayoutProps) {
+const RootLayout = ({ children }: RootLayoutProps) => {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
   );
-}
+};
+
+export default RootLayout;
