35b6502cfbd6241bdbc186868f30746cf307dc57
Ben <4991309+NuroDev@users.noreply.github.com>
2025-05-08 12:17:08 +0100

    Moved root layout into index page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import { use } from '@ronin/blade/server/hooks';
+import { useMetadata } from '@ronin/blade/server/hooks';
 
 import Link from '@components/link.client';
 import QuotationMark from '@components/quotation-mark';
@@ -6,30 +7,43 @@ import QuotationMark from '@components/quotation-mark';
 import type { MenuItems } from 'ronin';
 
 const Index = () => {
+  useMetadata({
+    title: 'Leo Lamprecht',
+    description: 'RONIN CEO. Previously 1st Head of Product at Vercel.',
+    icon: '/initials.svg',
+    x: {
+      creator: '@leo',
+    },
+  });
+
   const menuItems = use.menuItems.orderedBy.ascending(['ronin.updatedAt']) as unknown as MenuItems;
 
   return (
-    <main className="p-8 box-border relative min-h-full md:p-11">
-      <section className="md:absolute md:inset-0 md:text-center md:flex md:h-full md:items-center md:justify-center md:select-none md:cursor-default">
-        <h2 className="text-2xl hidden md:inline-block md:font-light md:mt-2 md:mr-10 md:ml-0 md:mb-0">
-          Just call me
-        </h2>
+    <html className="h-full" lang="en">
+      <body className="antialiased m-0 h-full">
+        <main className="p-8 box-border relative min-h-full md:p-11">
+          <section className="md:absolute md:inset-0 md:text-center md:flex md:h-full md:items-center md:justify-center md:select-none md:cursor-default">
+            <h2 className="text-2xl hidden md:inline-block md:font-light md:mt-2 md:mr-10 md:ml-0 md:mb-0">
+              Just call me
+            </h2>
 
-        <h1 className="text-6xl font-light m-0 absolute top-9 left-9 pr-9 md:relative md:inline-block md:left-auto md:top-auto md:pr-0 md:font-light md:text-7xl">
-          <QuotationMark position="left" />
-          Leo
-          <QuotationMark position="right" />
-        </h1>
-      </section>
+            <h1 className="text-6xl font-light m-0 absolute top-9 left-9 pr-9 md:relative md:inline-block md:left-auto md:top-auto md:pr-0 md:font-light md:text-7xl">
+              <QuotationMark position="left" />
+              Leo
+              <QuotationMark position="right" />
+            </h1>
+          </section>
 
-      <nav className="absolute left-7 bottom-7 flex flex-col md:left-8 md:right-8 md:bottom-8 md:flex-row md:justify-center thin:hidden">
-        {menuItems.map((menuItem) => (
-          <Link key={menuItem.id} href={menuItem.url} toggleEmail={menuItem.showEmail}>
-            {menuItem.name}
-          </Link>
-        ))}
-      </nav>
-    </main>
+          <nav className="absolute left-7 bottom-7 flex flex-col md:left-8 md:right-8 md:bottom-8 md:flex-row md:justify-center thin:hidden">
+            {menuItems.map((menuItem) => (
+              <Link key={menuItem.id} href={menuItem.url} toggleEmail={menuItem.showEmail}>
+                {menuItem.name}
+              </Link>
+            ))}
+          </nav>
+        </main>
+      </body>
+    </html>
   );
 };
 
