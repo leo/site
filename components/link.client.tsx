5b3df89ae8f726673deaa7238e59f83d4ea0bed7
Leo Lamprecht <leo@ronin.co>
2025-05-09 14:38:58 +0200

    Fixed client warning

--- components/link.client.tsx
+++ components/link.client.tsx
@@ -32,8 +32,8 @@ const Link = ({ children, href, toggleEmail }: LinkProps) => (
     href={href || twitterURL}
     target="_blank"
     rel="noreferrer"
-    onMouseEnter={toggleEmail && onMouseEnter}
-    onMouseLeave={toggleEmail && onMouseLeave}
+    onMouseEnter={toggleEmail ? onMouseEnter : undefined}
+    onMouseLeave={toggleEmail ? onMouseLeave : undefined}
     className="text-base no-underline text-black p-2.5 block md:rounded-md md:py-1 md:px-2.5 md:mx-1.5 md:transition md:duration-200 md:hover:bg-neutral-200/60"
   >
     {children}
