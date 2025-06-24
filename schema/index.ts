a4b2de1cff53e5336627c42c4f7c18345fb2bbf3
Leo Lamprecht <leo@ronin.co>
2025-06-24 19:32:24 +0200

    Replace `@ronin/blade` with `blade`

--- schema/index.ts
+++ schema/index.ts
@@ -1,4 +1,4 @@
-import { blob, boolean, model, string } from '@ronin/blade/universal/schema';
+import { blob, boolean, model, string } from 'blade/universal/schema';
 
 export const MenuItem = model({
   slug: 'menuItem',
